920750084491f60665baa5519ad8c51e22335237
Charles D Hamilton <lespaul36@gmail.com>
2018-03-02 02:09:41 -0800

    Starting project

---
+++ src/main/java/com/github/lespaul361/ebay1027stream/eBay1027OutputStream.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.lespaul361.ebay1027stream;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author David Hamilton
+ */
+public class eBay1027OutputStream extends OutputStream implements AutoCloseable{
+
+    @Override
+    public void write(int b) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
